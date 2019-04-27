3ca44d32c2598a3b55525b688f20dbac376d79ff
Tallulahwalz <tmwalznyc@gmail.com>
2019-04-27 15:28:37 +0000

    yay

--- warmupDay9.py
+++ warmupDay9.py
@@ -1,1 +1,30 @@
 # File name: warmupDay9.py
+list = [3,6,9,12]
+print(list)
+
+product = 1
+for x in list:
+    product *= x
+
+print(product)
+
+biggestnumber = 0
+for number in list:
+    if number > biggestnumber:
+        biggestnumber = number
+print(biggestnumber)
+
+
+word = input("what's your word?")
+
+print("you typed: " + word)
+
+words = input("what is your string")
+
+print("your string is: " + words)
+
+string = input("Enter your string: ")
+counter= 0
+for s in string:
+    counter = counter+1
+print("Length of input string is:", counter)
